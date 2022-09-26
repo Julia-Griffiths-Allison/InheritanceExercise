e32eb5c6904dd49104a5ea9828660501148ef1bb
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-26 11:14:53 -0400

    first commit, finished exercise

---
+++ Inheritance/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public string Breathing { get; set; }
+        public string Order { get; set; }
+        public string Tail { get; set; }
+        public string Longevity { get; set; }
+    }
+    public class Bird : Animal
+    {
+        public string Colors { get; set; }
+        public string Name { get; set; }
+        public string Diet { get; set; }
+        public string Size { get; set; }
+    }
+    public class Reptile : Animal
+    {
+        public string Name { get; set; }
+        public string Colors { get; set; }
+        public string Size { get; set; }
+        public string Diet { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,6 +6,35 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            Bird Parrot = new Bird()
+            {
+                Breathing = "alive and breathing",
+                Order = "Psittaciformes",
+                Tail = "feathered tail",
+                Longevity = "20-70 years",
+                Colors = "bright red, green, blue, yellow colors",
+                Name = "Parrots",
+                Diet = "fruits and veggies",
+                Size = "2 - 3.5 feet"
+            };
+
+            Console.WriteLine($"{ Parrot.Name} are {Parrot.Breathing}. Their technical order is {Parrot.Order} and they live about {Parrot.Longevity}. Some {Parrot.Name}s have {Parrot.Colors} with {Parrot.Tail}, they eat {Parrot.Diet} and are about {Parrot.Size}.");
+            Console.WriteLine("");
+            Reptile Python = new Reptile()
+            {
+                Breathing = "alive and breathing",
+                Order = "Squanata",
+                Tail = "Not sure where the tail starts, but it has one",
+                Longevity = "roughly 30 year maximum",
+                Name = "Ball Python",
+                Colors = "variety, although they're normally brown and black",
+                Size = "around 5' long",
+                Diet = "captive pets will normally eat dead mice, or other small rodents"
+            };
+
+            Console.WriteLine($"{Python.Name} are {Python.Breathing}. Their technical order is {Python.Order} and they live about {Python.Longevity}. Some {Python.Name}s have a {Python.Colors} of colors, they eat {Python.Diet} and are about {Python.Size}. {Python.Tail}.");
+
+
             // TODO Be sure to follow best practice when creating your classes
 
             // Create a class Animal
